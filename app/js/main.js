bd4dd757a5f8234ed80c57bdd92f823135ce2fc0
Sergey Kopelchuk <kopel@aport.ru>
2017-05-08 13:58:30 +0300

    Bread Crumbs, Page title and a little bit of media

---
+++ app/js/main.js
@@ -0,0 +1,82 @@
+/****************/
+//DomiraTV Engine
+/***************/
+$(document).ready(function () {
+
+    var tag = document.createElement('script');
+    tag.src = "//www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
+    var array = [];
+    $('.player').each(function () {
+        var val = $(this).data('id');
+        array.push(val);
+    });
+
+
+    var p = document.getElementsByClassName("player");
+    $(p).hide();
+
+
+    for(var i=0,j=1;i<= array.length-1, j<= array.length;i++, j++) {
+        var t = document.getElementById("thumbnail"+j);
+        t.src = "https://img.youtube.com/vi/"+ array[i] +"/0.jpg";
+    }
+
+
+    onPlayerStateChange = function (event) {
+        if (event.data == YT.PlayerState.ENDED) {
+            $('.start-video').fadeIn('normal');
+        }
+    };
+
+
+    $('.start-video').click(function () {
+        $(this).hide();
+        var wrap_id = $(this).data('id');
+        $("#" + wrap_id + " .player").show();
+        // $("#" + wrap_id + " .thumbnail_container .thumbnail").hide();
+        $("#" + wrap_id + " .thumbnail_container .start-video").hide();
+        $("#" + wrap_id + " .thumbnail_container").css("visibility", "hidden");
+
+        $("#" + wrap_id + " .movie-media-control").show().click( function () {
+            $("#" + wrap_id + " .start-video").show();
+            $(this).hide();
+        });
+        // player.playVideo()
+    });
+
+
+    $('.movie-media-video').dblclick(function () {
+        var link_id = $(this).find(".player").data("id");
+        window.open("https://www.youtube.com/watch?v=" + link_id);
+    });
+
+
+
+});
+
+function callPlayer(frame_id, func, args) {
+    if (window.jQuery && frame_id instanceof jQuery) frame_id = frame_id.get(0).id;
+    var iframe = document.getElementById(frame_id);
+    if (iframe && iframe.tagName.toUpperCase() != 'IFRAME') {
+        iframe = iframe.getElementsByTagName('iframe')[0];
+    }
+    if (iframe) {
+        // Frame exists,
+        iframe.contentWindow.postMessage(JSON.stringify({
+            "event": "command",
+            "func": func,
+            "args": args,
+            "id": frame_id,
+        }), "*");
+    }
+}
+
+
+
+/********************/
+//End DomiraTV Engine
+/*******************/
